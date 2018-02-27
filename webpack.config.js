ad7f2c8944a8a85d5d7bc30dd10b9af30ee21d63
ely405 <gonzalesm405@gmail.com>
2018-02-27 17:38:24 -0500

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,8 @@ const path = require('path');
 const webpack = require('webpack');
 const precss = require('precss');
 const autoprefixer = require('autoprefixer');
-// const combineLoaders = require('webpack-combine-loaders');
 
 module.exports = {
-	// devtool: 'inline-source-map',
 	devtool: 'eval-source-map',
 	entry: [
 		'react-hot-loader/patch',
@@ -23,61 +21,27 @@ module.exports = {
 				exclude: /node_modules/,
 				use: ['babel-loader', 'eslint-loader'],
 			},
-			// {
-			// 	test: /\.scss$/,
-			// 	loaders: ['style-loader', 'css-loader', 'sass-loader']
-			// },
 			{
 				test: /\.(scss)$/,
 				use: [{
-					loader: 'style-loader', // inject CSS to page
+					loader: 'style-loader', 
 				}, {
-					loader: 'css-loader', // translates CSS into CommonJS modules
+					loader: 'css-loader',
 				}, {
-					loader: 'postcss-loader', // Run post css actions
+					loader: 'postcss-loader',
 					options: {
-						plugins() { // post css plugins, can be exported to postcss.config.js
+						plugins() {
 							return [
 								precss, autoprefixer,
 							];
 						},
 					},
 				}, {
-					loader: 'sass-loader', // compiles Sass to CSS
+					loader: 'sass-loader',
 				}],
 			},
 			{ test: /\.(woff|woff2|eot|ttf)$/, loader: 'url-loader?limit=100000' },
 			{ test: /\.(jpe?g|png|gif|svg)$/i, loader: 'file-loader?name=img/[name].[ext]' },
-			// {
-			// 	test: /\.(gif|png|jpe?g|svg)$/i,
-			// 	use: [
-			// 	  'file-loader',
-			// 	  {
-			// 			loader: 'image-webpack-loader',
-			// 			options: {
-			// 		  mozjpeg: {
-			// 					progressive: true,
-			// 					quality: 65,
-			// 		  },
-			// 		  // optipng.enabled: false will disable optipng
-			// 		  optipng: {
-			// 					enabled: false,
-			// 		  },
-			// 		  pngquant: {
-			// 					quality: '65-90',
-			// 					speed: 4,
-			// 		  },
-			// 		  gifsicle: {
-			// 					interlaced: false,
-			// 		  },
-			// 		  // the webp option will enable WEBP
-			// 		  webp: {
-			// 					quality: 75,
-			// 		  },
-			// 			},
-			// 	  },
-			// 	],
-			//   },
 		],
 	},
 	resolve: {
@@ -95,7 +59,6 @@ module.exports = {
 			$: 'jquery',
 			jquery: 'jquery',
 		}),
-		// new ExtractTextPlugin('style.min.css', { allChunks: true }),
 	],
 	devServer: {
 		contentBase: './dist',
